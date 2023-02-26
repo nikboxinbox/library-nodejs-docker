c1f5b944729de8577ef38f6576370959226a1f81
nik_nik <nikbox88@gmail.com>
2023-02-26 22:19:51 +0300

    + services mongo, mongo express in docker-compose.yaml && mongoose.connect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Book = require("./models/Book");
+const mongoose = require("mongoose");
 
 // const loggerMiddleware = require("./middleware/logger");
 const errorMiddleware = require("./middleware/error");
@@ -33,6 +34,27 @@ app.use(errorMiddleware);
 app.use(internalServerErrorMiddleware);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running, go to http://localhost:${PORT}`);
-});
+const UserDB = process.env.DB_USERNAME || "root";
+const PasswordDB = process.env.DB_PAWORD || "qwerty12345";
+const NameDB = process.env.DB_Name || "books";
+const HostDb = process.env.DB_HOST || "mongodb://localhost:27017/";
+
+async function start() {
+  try {
+    await mongoose.connect(HostDb, {
+      user: UserDB,
+      pass: PasswordDB,
+      dbName: NameDB,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running, go to http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+start();
